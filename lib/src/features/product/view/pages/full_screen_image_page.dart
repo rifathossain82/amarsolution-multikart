897a57ee32455a941aa51542331a516279d32080
rifathossain82 <officialrifat82@gmail.com>
2024-08-11 16:49:08 +0600

    update: full screen image page

--- lib/src/features/product/view/pages/full_screen_image_page.dart
+++ lib/src/features/product/view/pages/full_screen_image_page.dart
@@ -6,23 +6,43 @@ import 'package:amarsolution_multikart/src/core/utils/color.dart';
 import 'package:amarsolution_multikart/src/core/widgets/cached_network_image_builder.dart';
 import 'package:amarsolution_multikart/src/features/product/model/product_details_model.dart';
 
-class FullScreenImagePage extends StatelessWidget {
+class FullScreenImagePage extends StatefulWidget {
   final List<Photo> photos;
 
-  FullScreenImagePage({
+  const FullScreenImagePage({
     super.key,
     required this.photos,
   });
 
-  final PageController controller = PageController(
-    viewportFraction: 1,
-    keepPage: true,
-  );
+  @override
+  _FullScreenImagePageState createState() => _FullScreenImagePageState();
+}
+
+class _FullScreenImagePageState extends State<FullScreenImagePage> {
+  late PageController controller;
+  late FullScreenImageController fullScreenImageController;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = PageController(
+      viewportFraction: 1,
+      keepPage: true,
+      initialPage: 0,
+    );
+
+    fullScreenImageController = Get.find<FullScreenImageController>()
+      ..updateIndex(0);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final fullScreenImageController = Get.put(FullScreenImageController());
-
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 0.0,
@@ -33,100 +53,168 @@ class FullScreenImagePage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            /// Close button
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  InkWell(
-                    onTap: () => Get.back(),
-                    child: Container(
-                      alignment: Alignment.centerRight,
-                      height: 30,
-                      width: 30,
-                      margin: const EdgeInsets.only(
-                        top: 0,
-                      ),
-                      decoration: const BoxDecoration(
-                        color: kWhite,
-                        shape: BoxShape.circle,
-                      ),
-                      child: const Center(
-                        child: Icon(
-                          Icons.close,
-                          size: 18,
-                          color: kBlack,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+            const _CloseButton(),
+            _LargeImageView(
+              pageController: controller,
+              photos: widget.photos,
+              onPageChanged: (int index) {
+                fullScreenImageController.updateIndex(index);
+              },
             ),
-
-            /// Large image
-            Expanded(
-              child: PageView.builder(
-                controller: controller,
-                itemCount: photos.length,
-                onPageChanged: (int index) {
+            _HorizontalImageList(
+              photos: widget.photos,
+              onTapImage: (int index) {
+                setState(() {
+                  controller.jumpToPage(index);
                   fullScreenImageController.updateIndex(index);
-                },
-                itemBuilder: (context, index) => Container(
-                  color: kBlackLight,
-                  width: double.infinity,
-                  child: CachedNetworkImageBuilder(
-                    imgURl: photos[index].image ?? '',
-                    borderRadius: BorderRadius.zero,
-                    backgroundColor: kBlack,
-                  ),
-                ),
-              ),
-            ),
-
-            /// Bottom image list
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-              child: SizedBox(
-                height: 60,
-                child: GridView.builder(
-                  physics: const ScrollPhysics(),
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  itemCount: photos.length,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 1,
-                    mainAxisSpacing: 5,
-                  ),
-                  itemBuilder: (context, int index) {
-                    return InkWell(
-                      onTap: () {
-                        controller.jumpToPage(index);
-                        fullScreenImageController.updateIndex(index);
-                      },
-                      child: Container(
-                        width: 40,
-                        height: 80,
-                        decoration: BoxDecoration(
-                          color: kGreyLight,
-                          border: Border.all(
-                            width: 1,
-                            color: 5 == index ? kPrimaryColor : kWhite,
-                          ),
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                        child: CachedNetworkImageBuilder(
-                          imgURl: photos[index].image ?? '',
-                          borderRadius: BorderRadius.zero,
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ),
+                });
+              },
+            )
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CloseButton extends StatelessWidget {
+  const _CloseButton();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 10,
+        vertical: 20,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          InkWell(
+            onTap: () => Get.back(),
+            child: Container(
+              alignment: Alignment.centerRight,
+              height: 30,
+              width: 30,
+              margin: const EdgeInsets.only(
+                top: 0,
+              ),
+              decoration: const BoxDecoration(
+                color: kWhite,
+                shape: BoxShape.circle,
+              ),
+              child: const Center(
+                child: Icon(
+                  Icons.close,
+                  size: 18,
+                  color: kBlack,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _LargeImageView extends StatelessWidget {
+  final PageController pageController;
+  final List<Photo> photos;
+  final Function(int index) onPageChanged;
+
+  const _LargeImageView({
+    required this.pageController,
+    required this.photos,
+    required this.onPageChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: PageView.builder(
+        controller: pageController,
+        itemCount: photos.length,
+        onPageChanged: onPageChanged,
+        itemBuilder: (context, index) => Container(
+          color: kBlackLight,
+          width: double.infinity,
+          child: CachedNetworkImageBuilder(
+            imgURl: photos[index].image ?? '',
+            borderRadius: BorderRadius.zero,
+            backgroundColor: kBlack,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _HorizontalImageList extends StatelessWidget {
+  final List<Photo> photos;
+  final Function(int index) onTapImage;
+
+  const _HorizontalImageList({
+    required this.photos,
+    required this.onTapImage,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.find<FullScreenImageController>();
+
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 10,
+        vertical: 20,
+      ),
+      child: SizedBox(
+        height: 60,
+        child: GridView.builder(
+          physics: const ScrollPhysics(),
+          shrinkWrap: true,
+          scrollDirection: Axis.horizontal,
+          itemCount: photos.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 1,
+            mainAxisSpacing: 5,
+          ),
+          itemBuilder: (context, int index) {
+            return Obx(() {
+              return InkWell(
+                onTap: () => onTapImage(index),
+                child: Container(
+                  width: 40,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    color: kGreyLight,
+                    border: Border.all(
+                      width: 3,
+                      color: controller.currentIndex.value == index
+                          ? kPrimaryColor
+                          : kGrey,
+                    ),
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      CachedNetworkImageBuilder(
+                        imgURl: photos[index].image ?? '',
+                        borderRadius: BorderRadius.circular(3),
+                      ),
+                      if (controller.currentIndex.value == index)
+                        const Icon(
+                          Icons.check,
+                          color: kPrimaryColor,
+                          size: 35,
+                        ),
+                    ],
+                  ),
+                ),
+              );
+            });
+          },
         ),
       ),
     );
